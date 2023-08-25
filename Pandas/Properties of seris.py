904d949c45d335c44b26876e67263effeb9eadb0
Omprakash Rawat <omprakash824rawat@gmail.com>
2023-08-25 23:40:27 +0530

    Leaning transpose of table

--- Pandas/Properties of seris.py
+++ Pandas/Properties of seris.py
@@ -2,3 +2,9 @@ import pandas as pd
 
 table = pd.read_csv("Richest Persons.csv")
 print(table)
+print("Table Information")
+print("Column are", table.columns)
+
+print()
+
+print(table.describe())
---
+++ Pandas/Transpose of table.py
@@ -0,0 +1,15 @@
+import numpy as np
+import pandas as pd
+
+np.random.seed(762)
+marks = np.random.randint(50,101,(10,5))
+
+Student = ['Omprakash', 'Vinayak', 'Kapil', 'Rishabh', 'Dhananjay', 'Vidit', 'Suresh', 'Maduri', 'Aastha', 'Chinmay']
+Subjects = ['Physics', 'Chemistry', 'Computer Science', 'Maths', 'Bio']
+
+result = pd.DataFrame(marks, Student, Subjects)
+
+print(result)
+
+print("\n\n\n")
+print(result.transpose())
